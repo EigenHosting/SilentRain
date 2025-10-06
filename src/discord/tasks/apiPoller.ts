0e920ce283923d199b1d58ca1f0f21dfaa3523b2
Melkinator <aacrazykid@yahoo.com>
2025-10-06 12:36:30 +0700

    fixed status and added memory percentage

--- src/discord/tasks/apiPoller.ts
+++ src/discord/tasks/apiPoller.ts
@@ -1,18 +1,27 @@
 import { Client, TextChannel, EmbedBuilder } from "discord.js";
+import { create } from "domain";
 
 const CHANNEL_ID = "1420299691681382471";
 const MESSAGE_ID = "1423126345067532510";
 const API_URL = "http://eigen-api.krulaor.org/status";
+
+function createProgressBar(percentage: number, length: 10): string {
+    const filledLength = Math.round(length*percentage/100);
+    const bar = "🟩".repeat(filledLength)+"🟥".repeat(length-filledLength);
+    return `[${bar}] ${percentage}%`;
+}
 
 export function startApiPoller(client: Client) {
     async function updateMessage() {
         try {
             const response = await fetch(API_URL);
             const data = await response.json();
+            const memoryPercent = data.message.items[0].info.mp ?? 0;
+            const progressBarText = createProgressBar(memoryPercent,10);
 
             const embed = new EmbedBuilder()
                 .setTitle("API Status")
-                .setDescription(`Status: **${data.status ?? "Unknown"}**`)
+                .setDescription(`Status: **${data.message.items[1].status ?? "Unknown"}**\n`+`Memory Percentage: ${progressBarText}`)
                 .setColor("Blurple")
                 .setTimestamp();
 
